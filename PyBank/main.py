5dbf143c64a73cc56a4b4e9d59d33c53e6a2ebcf
N Aldrich <naldrich@hs-27306.attlocal.net>
2021-01-19 23:23:56 -0600

    cant open csv

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,42 +1,43 @@
 import os
 import csv
 
-pybank_csv = os.path.join("/Users/naldrich/Desktop/python-challenge/PyBank/Resources/budget_data.csv")
-
+csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
 #Open and read csv
-with open("pybank_csv", "r") as csv_file:
-    csv_reader =csv.reader(csv_file, delimiter=",")
-    
+with open(csvpath) as csvfile:
+    csvreader =csv.reader(csvfile, delimiter=",")
+    print(csvreader)
+
     #Read the header row first 
-    csv_header = next(csv_file)
+    csv_header = next(csvreader)
     print(f"Header: {csv_header}")
 
 #Create lists to store months and profit 
-months = []
-profit = []
+#months = []
+#profit = []
 #Read through each row of data after the header 
 for row in csv_reader: 
+    print(csvreader)
     
-    months.append(row[1])
-    profit.append(int(row[1]))
+    #months.append(row[1])
+    #profit.append(int(row[1]))
 
-    total_months = len(months)
-    print (f"Total Months: {total_months}")
+    #total_months = len(months)
+    #print (f"Total Months: {total_months}")
 
-    revenues = 0
-    i = 1
-    for i in range(total):
-        revenues = revenues + int(profit[i])
-    print(f"Total: ${revenues}")
+    #revenues = 0
+    #i = 1
+   #for i in range(total):
+        #revenues = revenues + int(profit[i])
+    #print(f"Total: ${revenues}")
 
-    average_change = []
-    j = 0
-    k = 0
+    #average_change = []
+    #j = 0
+    #k = 0
 
-    for j in range (1, total_months):
-        if j == 0:
-            change.append(int(profit[j])-int(profit[k]))
-            k += 1
+    #for j in range (1, total_months):
+        #if j == 0:
+            #change.append(int(profit[j])-int(profit[k]))
+            #k += 1
 
 
 
