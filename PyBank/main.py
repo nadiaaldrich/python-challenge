2654e35fcdf347112df173aa0d608b42f4bfbed3
N Aldrich <naldrich@hs-27306.local>
2021-01-20 17:19:51 -0600

    saving changes to script

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,26 +2,28 @@ import os
 import csv
 
 
-csvpath = os.path.join("Resources", "budget_data.csv")
+csvpath = os.path.join("Resources/budget_data.csv")
 #Open and read csv
-with open(csvpath) as csvfile:
+with open(csvpath, newline="") as csvfile:
     csv_reader =csv.reader(csvfile, delimiter=",")
-    print(csv_reader)
+    
 
     #Read the header row first 
-    csv_header = next(csv_reader)
-    print(f"Header: {csv_header}")
+    csv_header = next(csv_reader, None)
+    
+#with open (csvpath, 'r') as csv_reader:
+    #lines = csv_reader.read()
 
 #Create lists to store months and profit 
-months = []
-profit = []
+    months = []
+    profit = []
 
 #Read through each row of data after the header 
-for row in csv_reader: 
-    print(csv_reader)
+    for row in csv_reader: 
     
-    months.append(row[1])
-    profit.append(int(row[1]))
+    
+        months.append(row[1])
+        profit.append(int(row[1]))
 
     total_months = len(months)
     print (f"Total Months: {total_months}")
@@ -29,12 +31,13 @@ for row in csv_reader:
     #add to get revenues
     revenues = 0
     i = 1
-   for i in range(total):
-        revenues = revenues + int(profit[i])
+    
+    for i in range(total_months):
+            revenues = revenues + int(profit[i])
     print(f"Total: ${revenues}")
 
     #find average change
-    average_change = []
+    change = []
     j = 0
     k = 0
 
@@ -51,16 +54,16 @@ for row in csv_reader:
 
 #greatest increase and decrease in profits 
     max_change = max(change)
-    print(f"${max_change}")
+    #print(f"${max_change}")
 
     min_change = min(change)
-    print(f"${min_change")
+   # print(f"${min_change}")
 
     max_change = max(change)
     print(f"Greatest Increase in Profits: ${max_change}")
 
     min_change = min(change)
-    print(f"Greatest Decrese in Profits: ${min_change}")
+    print(f"Greatest Decrease in Profits: ${min_change}")
 
 
 
