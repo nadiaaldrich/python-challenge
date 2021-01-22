7b4bd447b74edb19db0eedc3f570f4f4494fa8d0
N Aldrich <naldrich@hs-27306.attlocal.net>
2021-01-21 21:41:24 -0600

    saving script

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -23,3 +23,32 @@ with open(csvpath, newline="")as csvfile:
 
     #Read through each row of data after the header
     for row in csv_reader:
+
+        if row[2] not in candidate_list:
+            count_votes += 1
+            candite_list.append(row[2])
+        else:
+            count_votes += 1
+    
+    i = 0
+    total_candidates = len(candidate_list)
+    candidate_votes = [0]*total_candidates
+
+    for i in range(total_candidates):
+        if row[2]==candidate_list[i]:
+            candidate_votes[i]+=1
+
+    max_vote = max(candidate_votes)
+    vote_index = candidate_votes.index(most_vote)
+    winner = candidate_list[vote_index]
+
+    #Percentage breakdown for each candidate
+
+    j = 0 
+    percent_of_votes = []
+
+    for j in range(total_candidates):
+        percent = round(candidate_votes[j]/count_votes * 100, 2)
+        percent_of_ votes.append(percent)
+    
+    
