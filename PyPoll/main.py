743f271e7f7db06d504f2a0ed8fa3027f41a70e1
N Aldrich <naldrich@hs-27306.attlocal.net>
2021-01-21 23:13:29 -0600

    saving script

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,27 +1,26 @@
 import os 
 import csv 
 
-
+#set path
 csvpath = os.path.join("Resources/election_data.csv")
 
 #Open and read csv
 with open(csvpath, newline="")as csvfile:
-    csv_reader = csv.reader(csvfile, delimiter=",")
+    csv_reader = csv.reader (csvfile, delimiter=",")
+    
 
     #Read and skip the header (first row)
     csv_header = next(csv_reader, None)
 
 
 
-
     #Create lists and variable 
+    
     candidate_list = []
     count_votes = 0
 
     print(f"Election Results")
-    print("----------------------------------------")
-
-    #Read through each row of data after the header
+    
     for row in csv_reader:
 
         if row[2] not in candidate_list:
@@ -29,34 +28,32 @@ with open(csvpath, newline="")as csvfile:
             candidate_list.append(row[2])
         else:
             count_votes += 1
-    
+
     i = 0
-    total_candidates = len(candidate_list)
+    total_candidates = len(candidate_list) 
     candidate_votes = [0]*total_candidates
 
     for i in range(total_candidates):
+        
         if row[2]==candidate_list[i]:
             candidate_votes[i]+=1
 
+    max_vote = max(candidate_votes)
+    vote_index = candidate_votes.index(max_vote)
     max_votes = max(candidate_votes)
-    vote_index = candidate_votes.index(max_votes)
     winner = candidate_list[vote_index]
 
     #Percentage breakdown for each candidate
 
-    j = 0 
+    j = 0
     percent_of_votes = []
-
     for j in range(total_candidates):
-        percent = round(candidate_votes[j]/count_votes * 100, 2)
+        percent = round((candidate_votes[j]/count_votes)* 100, 2)
         percent_of_votes.append(percent)
+        
 
     print(f"Total Votes:{count_votes}")
     print("----------------------------------------")
     print(f"Name:{candidate_list}{percent_of_votes}, {candidate_votes}")
     print("----------------------------------------")
     print(f"Winner:{winner}")
-
-    
-    
-
